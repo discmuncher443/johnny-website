40f808dcd85d3ce86cf1548badb989c88bb36315
Johnny Sayle <johnny.sayle@impact.com>
2026-05-25 13:17:50 -0400

    fixing page links

--- scripts/strava.js
+++ scripts/strava.js
@@ -155,7 +155,7 @@ const displayActivities = (activities) => {
     });
 };
 
-fetch('activities.json')
+fetch('../activities.json')
 .then(response => {
     if (!response.ok) throw new Error('Network response was not ok');
     return response.json();
